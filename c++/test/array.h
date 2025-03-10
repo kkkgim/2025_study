568a650f499a2b7b23ccea3f27d5d0f36623b418
김태린 <taerin@gimtaelin-ui-noteubug.local>
2025-03-10 18:33:52 +0900

    다차원 배열

--- c++/test/array.h
+++ c++/test/array.h
@@ -1,36 +1,57 @@
 #include <iostream>
 
+class Array;  // 전방 선언
+
+// 🔹 다차원 인덱싱을 위한 보조 클래스
+class ArrayAccessor {
+    Array& arr;   // 원본 배열 참조
+    int index;    // 현재 접근한 차원의 인덱스
+    int depth;    // 현재 차원 (0부터 시작)
+
+public:
+    ArrayAccessor(Array& array, int idx, int d) : arr(array), index(idx), depth(d) {}
+
+    // 🔹 operator[]을 재귀적으로 호출하여 다차원 인덱싱 지원
+    ArrayAccessor operator[](int idx);
+    operator int&();  // 마지막 차원에서 int 값 반환
+};
+
+// 🔹 다차원 배열을 관리하는 클래스
 class Array {
     int* data;      // 1D 배열로 저장
     int* sizes;     // 각 차원의 크기를 저장하는 배열
     int dimensions; // 차원 개수
+    int totalSize;  // 전체 요소 개수
 
-    public:
-        template<typename ... Types>
-        Array(Types ... args);
-        ~Array();
+public:
+    template<typename... Types>
+    Array(Types... args);
 
-        void print();
-        // void operator[](int x);
+    ~Array();
+
+    void print();
+
+    ArrayAccessor operator[](int index) { return ArrayAccessor(*this, index, 0); }
+
+    friend class ArrayAccessor;  // ArrayAccessor가 private 멤버 접근 가능
 };
 
-template<typename ... Types>
+// 🔹 생성자: 가변 인자로 차원 크기 받음
+template<typename... Types>
 Array::Array(Types... args) : dimensions(sizeof...(args)), totalSize(1) {
     // 차원 크기를 배열에 저장
     sizes = new int[dimensions];
     int idx = 0;
     int temp[] = { args... };  // 가변 인자 배열로 저장
-
     for (int value : temp) {
         sizes[idx++] = value;
         totalSize *= value;  // 전체 크기 계산
     }
-    
 
     // 1D 배열 동적 할당
     data = new int[totalSize];
 
-    // 초기값 설정 (예제)
+    // 초기값 설정
     for (int i = 0; i < totalSize; i++) {
         data[i] = i + 1;
     }
@@ -42,14 +63,39 @@ Array::Array(Types... args) : dimensions(sizeof...(args)), totalSize(1) {
     std::cout << " created.\n";
 }
 
-
-Array::~Array(){
-    for(int i=0;i<length;i++){
-        delete[] arr[i];
-    } 
+// 🔹 소멸자: 동적 할당한 메모리 해제
+Array::~Array() {
+    delete[] data;
+    delete[] sizes;
+    std::cout << "Array destroyed.\n";
 }
 
+// 🔹 배열 요소 출력
+void Array::print() {
+    std::cout << "Array elements: ";
+    for (int i = 0; i < totalSize; i++) {
+        std::cout << data[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
-int main(){
-    Array a(2,2,2);
+// 🔹 다차원 인덱싱 연산자 오버로딩 (ArrayAccessor)
+ArrayAccessor ArrayAccessor::operator[](int idx) {
+    return ArrayAccessor(arr, index * arr.sizes[depth + 1] + idx, depth + 1);
+}
+
+// 🔹 마지막 차원에서 `int&` 반환
+ArrayAccessor::operator int&() {
+    return arr.data[index];
+}
+
+int main() {
+    Array a(2, 2, 2);  // 2x2x2 배열 생성
+    a.print();
+
+    // 🔹 다차원 인덱싱 지원
+    a[1][1][1] = 99;  
+    std::cout << "Modified element a[1][1][1]: " << a[1][1][1] << std::endl;
+
+    return 0;
 }
